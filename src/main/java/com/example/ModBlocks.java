16f10dc9618b80cdf81464a9383fe7c13a50fa5d
pf4 <74263128+elydre@users.noreply.github.com>
2026-09-09 19:53:20 +0200

    aled

---
+++ src/main/java/com/example/ModBlocks.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import java.util.function.Supplier;
+
+import com.example.blocks.aledblock;
+
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.block.Block;
+
+public class ModBlocks {
+
+	private static Block register(String name, Supplier<Block> blockFactory) {
+		ResourceKey<Block> blockKey = keyOfBlock(name);
+		Block block = blockFactory.get();
+
+		return Registry.register(BuiltInRegistries.BLOCK, blockKey, block);
+	}
+
+	public static final Block ALED_BLOCK = register("aled", aledblock::new);
+
+	public static ResourceKey<Block> keyOfBlock(String name) {
+		return ResourceKey.create(Registries.BLOCK, Identifier.fromNamespaceAndPath(ExampleMod.MOD_ID, name));
+	}
+
+	public static void initialize() {}
+}
---
+++ src/main/java/com/example/blocks/aledblock.java
@@ -0,0 +1,33 @@
+package com.example.blocks;
+
+import com.example.ModBlocks;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.level.Level;
+
+public class aledblock extends Block {
+    
+    public aledblock() {
+        super(BlockBehaviour.Properties.of().setId(ModBlocks.keyOfBlock("plate")).sound(SoundType.DEEPSLATE).noOcclusion());
+    }
+
+
+
+    @Override
+    protected InteractionResult useItemOn(ItemStack itemStack, BlockState state, Level level, BlockPos pos,
+            Player player, InteractionHand hand, BlockHitResult hitResult) {
+        // please stfu copilot
+        player.getInventory().add(new ItemStack(Items.DIAMOND, 3));
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        return InteractionResult.SUCCESS;
+    }
+}
