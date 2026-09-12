5d206ce1d22c6f33a8507b89904e539794f99d54
pf4 <74263128+elydre@users.noreply.github.com>
2026-09-12 18:50:53 +0200

    Update ComputerScreen.java

--- src/client/java/com/example/client/ComputerScreen.java
+++ src/client/java/com/example/client/ComputerScreen.java
@@ -1,12 +1,19 @@
 package com.example.client;
 
 import com.example.networking.ComputerClosePayload;
+import com.example.networking.ComputerKeyPayload;
 
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+
+import net.fabricmc.fabric.api.client.screen.v1.ScreenKeyboardEvents;
+import net.minecraft.client.input.KeyEvent;
+import net.minecraft.client.input.CharacterEvent;
+
+import org.lwjgl.glfw.GLFW;
 
 public class ComputerScreen extends Screen {
 
@@ -15,6 +22,20 @@ public class ComputerScreen extends Screen {
     public ComputerScreen(BlockPos computerPos) {
         super(Component.literal("aledblock"));
         this.computerPos = computerPos;
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+
+        ScreenKeyboardEvents.beforeKeyPress(this)
+                .register(this::onKeyPress);
+
+        ScreenKeyboardEvents.beforeCharType(this)
+                .register(this::onCharType);
+
+        ScreenKeyboardEvents.beforeKeyRelease(this)
+                .register(this::onKeyRelease);
     }
 
     @Override
@@ -46,6 +67,51 @@ public class ComputerScreen extends Screen {
                     false
             );
         }
+    }
+
+    private void onKeyPress(Screen screen, KeyEvent event) {
+        char key = switch (event.key()) {
+            case GLFW.GLFW_KEY_BACKSPACE -> '\b';
+            case GLFW.GLFW_KEY_ENTER -> '\r';
+            case GLFW.GLFW_KEY_TAB -> '\t';
+            default -> 0;
+        };
+
+        if (key == 0) {
+            return;
+        }
+
+        for (int i = 1; i <= 2; i++) {
+            ClientPlayNetworking.send(
+                    new ComputerKeyPayload(
+                            computerPos,
+                            (i << 16) | (key & 0xFFFF)
+                    )
+            );
+        }
+    }
+
+    private void onCharType(Screen screen, CharacterEvent event) {
+        String text = event.codepointAsString();
+
+        if (text.isEmpty()) {
+            return;
+        }
+
+        int key = text.charAt(0);
+
+        for (int i = 1; i <= 2; i++) {
+            ClientPlayNetworking.send(
+                    new ComputerKeyPayload(
+                            computerPos,
+                            (i << 16) | (key & 0xFFFF)
+                    )
+            );
+        }
+    }
+
+    private void onKeyRelease(Screen screen, KeyEvent event) {
+        // todo
     }
 
     @Override
